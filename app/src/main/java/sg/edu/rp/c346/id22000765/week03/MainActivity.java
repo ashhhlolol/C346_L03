ceea9e6f7b2a28a6af7d0780c81b8f588d05e117
Ashley <22000765@myrp.edu.sg>
2023-05-04 14:01:40 +0800

    Completed up to section C

--- app/src/main/java/sg/edu/rp/c346/id22000765/week03/MainActivity.java
+++ app/src/main/java/sg/edu/rp/c346/id22000765/week03/MainActivity.java
@@ -6,7 +6,9 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.RadioGroup;
 import android.widget.TextView;
+import android.widget.ToggleButton;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -14,6 +16,8 @@ public class MainActivity extends AppCompatActivity {
     TextView tvDisplay;
     Button btnDisplay;
     EditText etInput;
+    ToggleButton tbtn;
+    RadioGroup rgGender;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,6 +27,9 @@ public class MainActivity extends AppCompatActivity {
         tvDisplay = findViewById(R.id.textViewDisplay);
         btnDisplay = findViewById(R.id.buttonDisplay);
         etInput = findViewById(R.id.editTextInput);
+        tbtn = findViewById(R.id.toggleButtonEnabled);
+        rgGender =findViewById(R.id.rgGender);
+
 
         btnDisplay.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -30,6 +37,61 @@ public class MainActivity extends AppCompatActivity {
                 //code for the action
                 String stringResponse=etInput.getText().toString();
                 tvDisplay.setText((stringResponse));
+                tbtn.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        // Add your code for the action
+
+                        // get the state of the toggle button
+                        //when the toggle button is unchecked, disable the EditText
+                        //when the toggle button is checked enable the EditText to false
+                        //enable it when it is checked
+                        boolean isChecked = tbtn.isChecked();
+                        if (isChecked) {
+                            etInput.setEnabled(true);
+                        }else{
+                            etInput.setEnabled(false);
+                        }
+
+
+                        btnDisplay.setOnClickListener(new View.OnClickListener() {
+                            @Override
+                            public void onClick(View v) {
+                                // Code for the action
+                                String stringResponse = etInput.getText().toString();
+                                int checkedRadioId = rgGender.getCheckedRadioButtonId();
+
+                                String Display;
+                                if(checkedRadioId == R.id.radioButtonGenderMale){
+                                    // Write the code when male selected
+                                    Display = "He says ";
+
+                                } else{
+                                    // Write the code when female selected
+                                    Display ="She says ";
+
+                                }
+                                String displayText =  Display + stringResponse;
+
+                                tvDisplay.setText(displayText);
+                            }
+                        });
+
+
+
+
+
+
+
+
+
+
+
+
+
+                    }
+                });
+
 
             }
         });
